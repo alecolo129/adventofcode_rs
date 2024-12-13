c7919c415666affd2ee8450a1d6f42cf3f513430
Alessandro Colombo <alessandro.colombo@epfl.ch>
2024-12-13 11:08:04 +0100

    Add day eleven

--- day_eleven/src/main.rs
+++ day_eleven/src/main.rs
@@ -1,7 +1,7 @@
-use std::collections::HashMap;
 /*
 You can find the challenge description here: https://adventofcode.com/2024/day/11
 */
+use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
@@ -26,7 +26,6 @@ fn parse(reader: &mut BufReader<File>) -> Result<HashMap<u64, u64>, Box<dyn Erro
 
 fn part_one(stones_to_count: &mut HashMap<u64, u64>, n_blinks: u32) -> u64 {
     (0..n_blinks).for_each(|i| {
-        // println!("\n{},{}: {:?}", i, stones_to_count.len(), stones_to_count);
         let stones_to_count_old = stones_to_count.clone();
         stones_to_count.clear();
 
@@ -34,18 +33,18 @@ fn part_one(stones_to_count: &mut HashMap<u64, u64>, n_blinks: u32) -> u64 {
             .iter()
             .for_each(|(num, count)| {
                 let n_digits = if *num != 0 { num.ilog10() + 1 } else { 1 };
+                
                 // Split number in 2
                 if n_digits > 0 && (n_digits % 2) == 0 {
                     let base = 10u64.pow(n_digits >> 1);
                     let rem = *num % base; //num[..n_digits >> 1].trim_start_matches('0').to_string();
                     let div = num / base; //num[n_digits >> 1..].trim_start_matches('0').to_string();
-                    // stones_to_count.remove(num);
                     stones_to_count.entry(rem).and_modify(|old_count| *old_count += *count).or_insert(*count);
                     stones_to_count.entry(div).and_modify(|old_count| *old_count += *count).or_insert(*count);
                 }
+                
                 // Replace 0s with 1s
                 else if *num == 0 {
-                    // stones_to_count.remove(num);
                     stones_to_count.entry(1).and_modify(|old_count| *old_count += *count).or_insert( *count);
                 }
 
@@ -55,8 +54,6 @@ fn part_one(stones_to_count: &mut HashMap<u64, u64>, n_blinks: u32) -> u64 {
                     stones_to_count.entry(num*2024).and_modify(|old_count| *old_count += *count).or_insert( *count); 
                 }
             });
-        // stones.append(&mut to_add);
-        // println!("{:?}", stones);
     });
     stones_to_count.iter().map(|(_, v)| *v).sum()
 }
